98c104c6a5a0f6a04fd8e5801bc6290badb27a78
Lecogoni <nicolas.giraud@me.com>
2020-09-29 21:58:40 +0200

    mise a jour fichier exo_20 avec la pyramide inversé

--- exo_20.rb
+++ exo_20.rb
@@ -2,8 +2,11 @@
     puts "combien d'étage veux tu ?"
 floor_number = gets.to_i
 
-
 floor_number.times do |i|
     puts "*" * (i + 1)
+end
+
+floor_number.times do |i|
+    puts " " * (floor_number - i) + "*" * (i + 1)
 end
 
